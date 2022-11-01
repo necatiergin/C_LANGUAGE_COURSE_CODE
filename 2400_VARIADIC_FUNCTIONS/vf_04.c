81ee6a01d063371ded3e1427c7beb8ef00e9e84e
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2022-11-01 16:59:10 +0300

    Create vf_04.c

---
+++ 2400_VARIADIC_FUNCTIONS/vf_04.c
@@ -0,0 +1,43 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdarg.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+char* concat(const char* ptr, ...)
+{
+	va_list args1, args2;
+
+	va_start(args1, ptr);
+	va_copy(args2, args1);
+	const char* p;
+
+	size_t len = strlen(ptr);
+	while ((p = va_arg(args1, const char*)) != NULL)
+		len += strlen(p);
+
+	char* pd = (char*)malloc(len + 1);
+	if (!pd) {
+		fprintf(stderr, "cannot allocate memory\n");
+		exit(EXIT_FAILURE);
+	}
+	va_end(args1);
+
+	strcpy(pd, ptr);
+
+	while ((p = va_arg(args2, const char*)) != NULL)
+		strcat(pd, p);
+
+	va_end(args2);
+
+	return pd;
+}
+
+int main(void)
+{
+	char* p = concat("necati ", "ergin ", "kaan ", "aslan", NULL);
+
+	puts(p);
+	free(p);
+}
