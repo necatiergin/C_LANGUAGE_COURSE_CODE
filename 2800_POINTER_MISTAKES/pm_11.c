53647e9accaa18851a63ba2e616da8305a4f71d5
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2022-11-23 21:29:08 +0300

    Create pm_11.c

---
+++ 2800_POINTER_MISTAKES/pm_11.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+int main(void)
+{
+	//...
+	size_t n = (rand() % 100 + 5) * 16;
+	int* p = (int*)malloc(sizeof(int)); // Let's call this memory block 1
+	//...
+	if (!p) {
+		printf("cannot allocate memory!");
+		return 1;
+	}
+	//...
+	int x = 20;
+	p = &x; //memory leak
+	//...
+}
