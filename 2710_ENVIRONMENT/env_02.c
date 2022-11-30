b23d1ffc3382b9ebe231112132d5530b2f083d6d
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2022-11-30 16:09:06 +0300

    Create env_02.c

---
+++ 2710_ENVIRONMENT/env_02.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    const char* const name = "OS";
+    const char* const env_path = getenv(name);
+    
+    if (env_path != NULL)
+        printf("%s = %s\n", name, env_path);
+}
