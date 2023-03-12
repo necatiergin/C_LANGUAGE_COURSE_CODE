a20feb15cdd02356a8a046b912d7937392ea695f
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-03-12 13:42:25 +0300

    Create endian_conversion_01.c

---
+++ 2000_BITWISE_OPERATIONS/endian_conversion_01.c
@@ -0,0 +1,8 @@
+#define encon32(x)				(((x) << 24) | ((x) >> 24) | ((x) >> 8 & 0x0000FF00) | ((x) << 8 & 0x00FF0000))
+
+int main(void)
+{
+	unsigned int uval = 0xAB12CFD8;
+
+	printf("%X  %X\n", uval, encon32(uval));
+}
