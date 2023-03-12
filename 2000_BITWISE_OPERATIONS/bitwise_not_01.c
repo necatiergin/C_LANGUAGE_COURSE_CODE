cbe2715e45350ecc6e6fee2287a97a0ef7bea76b
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-03-12 21:02:32 +0300

    Create bitwise_not_01.c

---
+++ 2000_BITWISE_OPERATIONS/bitwise_not_01.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <limits.h>
+
+void bit_print(int x)
+{
+	unsigned int mask = ~(~0u >> 1);
+
+	while (mask) {
+		putchar(x & mask ? '1' : '0');
+		mask >>= 1;
+	}
+	putchar('\n');
+}
+
+int main(void)
+{
+	unsigned int x = UINT_MAX;
+	unsigned int y = -1;
+	unsigned int z = ~0U;
+
+	bit_print(x);
+	bit_print(y);
+	bit_print(z);
+}
