8365bdfe1b7869d073ab11753c37478019df3cad
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-04-12 19:51:23 +0300

    Update sizeof_01.c

--- 0840_SIZEOF_OPERATOR/sizeof_01.c
+++ 0840_SIZEOF_OPERATOR/sizeof_01.c
@@ -5,7 +5,7 @@ int main(void)
 	printf("sizeof(char)		= %zu\n", sizeof(char));
 	printf("sizeof(_Bool)		= %zu\n", sizeof(_Bool));
 	printf("sizeof(short)		= %zu\n", sizeof(short));
-	printf("sizeof(int)			= %zu\n", sizeof(int));
+	printf("sizeof(int)		= %zu\n", sizeof(int));
 	printf("sizeof(long)		= %zu\n", sizeof(long));
 	printf("sizeof(long long)	= %zu\n", sizeof(long long));
 	printf("sizeof(float)		= %zu\n", sizeof(float));
