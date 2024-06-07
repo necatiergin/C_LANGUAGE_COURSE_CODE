486148cdb5fb86172f33a46e7095d65bfd599af5
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-06-07 12:03:09 +0300

    Create bitwise_and_01.c

---
+++ 2000_BITWISE_OPERATIONS/bitwise_and_01.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void bit_print(unsigned int x)
+{
+	unsigned int mask = ~(~0u >> 1);
+
+	while (mask) {
+		putchar(x & mask ? '1' : '0');
+		mask >>= 1;
+	}
+	putchar('\n');
+}
+
+void main(void)
+{
+	int x, y;
+
+	printf("enter two integers: ");
+	(void)scanf("%d%d", &x, &y);
+	printf("x     = ");
+	bit_print(x);
+	printf("y     = ");
+	bit_print(y);
+	printf("x & y = ");
+	bit_print(x & y);
+}
