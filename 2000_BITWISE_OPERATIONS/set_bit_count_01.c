6a088bb32ddc0f93ebccdeb36ddee03ec5ac9085
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-06-07 11:58:36 +0300

    Create set_bit_count_01.c

---
+++ 2000_BITWISE_OPERATIONS/set_bit_count_01.c
@@ -0,0 +1,26 @@
+// kernighan algorithm
+
+#include <stdio.h>
+
+
+int set_bit_count(int x)
+{
+	int cnt = 0;
+
+	while (x) {
+		x &= x - 1;
+		++cnt;
+	}
+
+	return cnt;
+}
+
+
+int main(void)
+{
+	int x;
+
+	printf("enter an integer: ");
+	scanf("%d", &x);
+	printf("set bit count is : %d\n", set_bit_count(x));
+}
