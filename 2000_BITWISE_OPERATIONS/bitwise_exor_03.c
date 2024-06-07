51e8dca70af239a2a68ec2b2ebb38f927bb00df4
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-06-07 17:47:15 +0300

    Create bitwise_exor_03.c

---
+++ 2000_BITWISE_OPERATIONS/bitwise_exor_03.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int x, y;
+
+	printf("enter to integers: ");
+	(void)scanf("%d%d", &x, &y);
+
+	printf("x = %d y = %d\n", x, y);
+	x ^= y, y ^= x, x ^= y;
+
+	printf("x = %d y = %d\n", x, y);
+};
