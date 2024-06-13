e3aa2f4a97b1c9826dccfc5cfe34fb0e96167cc4
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-06-13 12:16:34 +0300

    Create file_15.c

---
+++ 2200_FILE_OPERATIONS/file_15.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	if (argc != 2) {
+		fprintf(stderr, "usage: <fileprint> <filename>\n");
+		return 1;
+	}
+
+
+	FILE* f = fopen(argv[1], "r");
+	if (f == NULL) {
+		fprintf(stderr, "cannot open file to read!\n");
+		return 2;
+	}
+
+	int c;
+
+	while ((c = fgetc(f)) != EOF) {
+		putchar(c);
+	}
+
+	fclose(f);	
+}
