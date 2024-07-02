fd685fc50161b5476261364187c61cd4c75977af
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-07-02 13:40:06 +0300

    Create freopen_01.c

---
+++ 2740_STDC_FUNCTIONS/freopen_01.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    puts("stdout is printed to console");
+    if (freopen("redir.txt", "w", stdout) == NULL)
+    {
+        perror("freopen() failed");
+        return EXIT_FAILURE;
+    }
+    puts("stdout is redirected to a file"); // this is written to redir.txt
+    fclose(stdout);
+}
