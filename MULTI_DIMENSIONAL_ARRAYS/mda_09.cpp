a8ea59e9e6b570954062b8f6cb9d8ba323adf543
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-10-22 19:18:33 +0300

    Create mda_09.cpp

---
+++ MULTI_DIMENSIONAL_ARRAYS/mda_09.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a[5][10];
+
+	int* p1 = a; //wrong - implicit conversion
+	int(*p2)[10] = a; //correct
+	int* p3 = a[0]; //correct - array to pointer conversion
+	int* p4 = &a[0][0]; //correct 
+}
