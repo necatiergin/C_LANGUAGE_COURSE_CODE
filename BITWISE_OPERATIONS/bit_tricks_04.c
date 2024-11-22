f03f162bc8d971ec40cbd73ceae75678a0a5eda6
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-11-22 19:10:01 +0300

    Create bit_tricks_04.c

---
+++ BITWISE_OPERATIONS/bit_tricks_04.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+
+#include <stdio.h>
+
+int main()
+{
+    // Convert to lowercase
+    printf("'k' => '%c'\n", ('k' | ' '));
+    printf("'K' => '%c'\n", ('K' | ' '));
+
+    // Convert to uppercase
+    printf("'k' => '%c'\n", ('k' & '_'));
+    printf("'K' => '%c'\n", ('k' & '_'));
+}
