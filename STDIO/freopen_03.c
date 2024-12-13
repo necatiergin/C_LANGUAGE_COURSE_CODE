5f7697870d79cf136c4f5e96dba8684140ffa5eb
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-12-13 17:29:34 +0300

    Create freopen_03.c

---
+++ STDIO/freopen_03.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	FILE* f;
+
+	if ((f = freopen("test.txt", "w", stdout)) == NULL) {
+		fprintf(stderr, "cannot open file!..\n");
+		exit(EXIT_FAILURE);
+	}
+
+	for (int i = 0; i < 10; ++i)
+		printf("%d\n", i);
+}
