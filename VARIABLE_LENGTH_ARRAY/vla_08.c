f5ff35da4a2c841702c6e12ed069bbe034d531b3
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2026-02-15 07:52:58 +0300

    Update vla_08.c

--- VARIABLE_LENGTH_ARRAY/vla_08.c
+++ VARIABLE_LENGTH_ARRAY/vla_08.c
@@ -7,3 +7,16 @@ void mat_add(size_t r, size_t c,
         for (size_t j = 0; j < c; ++j)
             out[i][j] = a[i][j] + b[i][j];
 }
+
+int main(void)
+{
+    int main()
+    {
+        size_t row = 3, ccol = 4;
+        double a[row][col];
+        double b[row][col];
+        double c[row][col];
+        //...
+        mat_add(row, col, a, b, c);
+    }
+}
