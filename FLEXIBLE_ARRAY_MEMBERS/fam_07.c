d67e83f1bd5fc2755e6acb117504403890d690b6
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2026-02-15 07:32:15 +0300

    Create fam_07.c

---
+++ FLEXIBLE_ARRAY_MEMBERS/fam_07.c
@@ -0,0 +1,17 @@
+#include <limits.h>
+#include <string.h>
+
+struct String {
+    size_t len;
+    char data[];
+};
+
+int main(void)
+{
+    char src[] = "necati ergin aslan";
+    size_t len = strlen(src);
+    struct String* s = malloc(sizeof(*s) + len + 1);
+    //...
+    s->len = len;
+    memcpy(s->data, src, len + 1);
+}
