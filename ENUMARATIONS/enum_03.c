7f415b380313e9922c61650ab7a678271299022a
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2026-08-22 07:36:16 +0300

    Add ErrorCode enumeration and read_file function

---
+++ ENUMARATIONS/enum_03.c
@@ -0,0 +1,14 @@
+enum ErrorCode {
+    ERR_OK,
+    ERR_INVALID_ARGUMENT,
+    ERR_OUT_OF_MEMORY,
+    ERR_IO,
+    ERR_TIMEOUT
+};
+
+enum ErrorCode read_file(const char *filename)
+{
+    /* ... */
+
+    return ERR_OK;
+}
